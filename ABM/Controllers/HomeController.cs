03d84187a9d28ebddf901d9056bbf4020450e169
javiergespejo <javiergespejo@gmail.com>
2020-09-23 16:23:51 -0300

    Added HomeViewModel class

--- ABM/Controllers/HomeController.cs
+++ ABM/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using ABM.Models;
+using ABM.Repository;
+using ABM.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +12,27 @@ namespace ABM.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UnitOfWork _unitOfWork;
+
+        public HomeController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public ActionResult Index()
         {
+            //var projectList = _unitOfWork.PostRepository.Get().Where(p => p.is_active == true).ToList();
+            //var images = _unitOfWork.HomeRepository.GetImages();
+            //var welcomeText = _unitOfWork.HomeRepository.GetWelcomeText();
+
+            //HomeViewModel homeViewModel = new HomeViewModel
+            //{
+            //    SliderImage = images,
+            //    WelcomeText = welcomeText
+            //};
+
             return View();
+            //return View(projectList);
         }
 
         public ActionResult About()
---
+++ ABM/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ABM.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace ABM.ViewModels
+{
+    public class HomeViewModel
+    {        
+        public IEnumerable<byte[]> SliderImage { get; set; }
+        public string WelcomeText { get; set; }
+    }
+}
