719870ed5d44b242a9856a39833af83a0ffdff5c
Juan Sosa <sosajseba@gmail.com>
2020-09-23 16:37:13 -0300

    add home repository class and interface

---
+++ ABM/Interfaces/IHomeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ABM.Interfaces
+{
+    public class IHomeRepository : IDisposable
+    {
+        string GetWelcomeText();
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
