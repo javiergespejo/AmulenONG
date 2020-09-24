e902332ea08b3c169ae0830a023657f9d5734579
PatricioMolina <patomolina88@gmail.com>
2020-09-24 00:37:09 -0300

    Add projectController

---
+++ ABM/Controllers/proyectController.cs
@@ -0,0 +1,38 @@
+﻿using ABM.Repository;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ABM.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly IProjectRepository _projectRepository;
+
+
+        public ProjectController()
+        {
+            _projectRepository = new projectRepository;
+        }
+
+
+        //public ProjectController(IProjectRepository projectRepository)
+        //{
+        //  _projectRepository = projectRepository
+        //}
+
+        public ActionResult Home ()
+        {
+            var project = _projectRepository.GetActiveProjects;
+            return View(project);
+
+        }
+
+
+        
+    }
+}
