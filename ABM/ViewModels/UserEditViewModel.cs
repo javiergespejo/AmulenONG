90dfa13a88e653923396881b10341003f25abe72
javiergespejo <javiergespejo@gmail.com>
2020-09-29 18:39:14 -0300

    ViewModel for UserController Edit method added

---
+++ ABM/ViewModels/UserEditViewModel.cs
@@ -0,0 +1,47 @@
+﻿using ABM.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ABM.ViewModels
+{
+    public class UserEditViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName(displayName: "Nombre")]
+        public string Name { get; set; }
+        [DisplayName(displayName: "Correo electrónico")]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DisplayName(displayName: "Usuario")]
+        public string Username { get; set; }
+
+        // Parameterless constructor
+        public UserEditViewModel()
+        {
+        }
+
+        public UserEditViewModel(User user)
+        {
+            this.Id = user.id;
+            this.Name = user.name;
+            this.Email = user.email;
+            this.Username = user.username;
+        }
+
+        public User ToUserEntity()
+        {
+            return new User()
+            {
+                id = this.Id,
+                name = this.Name,
+                email = this.Email,
+                username = this.Username
+            };
+        }
+    }
+}
