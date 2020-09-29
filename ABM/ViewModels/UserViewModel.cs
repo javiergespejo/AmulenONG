4cd548aad015c0dfaa5a6f149a505e33aee311f8
PatricioMolina <patomolina88@gmail.com>
2020-09-29 14:25:22 -0300

    fix userViewModel

--- ABM/ViewModels/UserViewModel.cs
+++ ABM/ViewModels/UserViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ABM.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,18 @@ namespace ABM.ViewModels
 {
     public class UserViewModel
     {
-        public IEnumerable<User> users { get; set; }
+        [Display(Name = "Nombre completo")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
+        public string Name { get; set; }
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
+        [EmailAddress(ErrorMessage = "Dirección invalida")]
+        public string Email { get; set; }
+        [Display(Name = "Nombre de usuario")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
+        public string UserName { get; set; }
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
+        public string Pass { get; set; }
     }
 }
