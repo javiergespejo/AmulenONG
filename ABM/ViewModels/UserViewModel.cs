171967cf7cab301a26c018107171298c5d6ef6aa
PatricioMolina <patomolina88@gmail.com>
2020-09-30 14:22:11 -0300

    fox user viewModel spaces

--- ABM/ViewModels/UserViewModel.cs
+++ ABM/ViewModels/UserViewModel.cs
@@ -9,16 +9,21 @@ namespace ABM.ViewModels
 {
     public class UserViewModel
     {
+        public IEnumerable<User> users { get; set; }
+
         [Display(Name = "Nombre completo")]
         [Required(ErrorMessage = "Este campo es obligatorio!")]
         public string Name { get; set; }
+
         [Display(Name = "Email")]
         [Required(ErrorMessage = "Este campo es obligatorio!")]
         [EmailAddress(ErrorMessage = "Dirección invalida")]
         public string Email { get; set; }
+
         [Display(Name = "Nombre de usuario")]
         [Required(ErrorMessage = "Este campo es obligatorio!")]
         public string UserName { get; set; }
+
         [Display(Name = "Contraseña")]
         [Required(ErrorMessage = "Este campo es obligatorio!")]
         public string Pass { get; set; }
