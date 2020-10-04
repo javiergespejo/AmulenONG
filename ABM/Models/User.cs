b74ab5d4a2f2b7fff86e7a7fb890e94656ef34cc
Federico Galvan <fedegv.4991@gmail.com>
2020-10-04 18:57:44 -0300

    Add new field for User called ResetPasswordCode to verify Recover Password requests

--- ABM/Models/User.cs
+++ ABM/Models/User.cs
@@ -11,8 +11,7 @@ namespace ABM.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class User
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -20,14 +19,15 @@ namespace ABM.Models
         {
             this.SuscriptorProyect = new HashSet<SuscriptorProyect>();
         }
+    
         public int id { get; set; }
-
         public string name { get; set; }
         public string email { get; set; }
         public string username { get; set; }
         public string pass { get; set; }
         public int typeUserId { get; set; }
         public bool isActive { get; set; }
+        public string ResetPasswordCode { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<SuscriptorProyect> SuscriptorProyect { get; set; }
