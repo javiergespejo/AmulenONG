e4cac9801a5b5c96e0d85677120b7ac58932ee9e
Federico Galvan <fedegv.4991@gmail.com>
2020-10-23 11:48:50 -0300

    Add MercadoPago model and ViewModel

---
+++ ABM/ViewModels/MercadoPagoViewModel.cs
@@ -0,0 +1,37 @@
+﻿using ABM.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ABM.ViewModels
+{
+    public class MercadoPagoViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Monto")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
+        [DataType(DataType.Currency, ErrorMessage = "Debe ser de tipo numerico!")]
+        [Range(1, Int32.MaxValue, ErrorMessage = "El monto tiene que ser mayor a cero")]
+        public decimal Amount { get; set; }
+
+        [Display(Name = "Link de MercadoPago")]
+        [Required(ErrorMessage = "Este campo es obligatorio!")]
+        [DataType(DataType.Url, ErrorMessage = "Debe ser un direccion web valida!")]
+        public string Link { get; set; }
+
+        public MercadoPagoButton ToEntity()
+        {
+            MercadoPagoButton mpButton = new MercadoPagoButton
+            {
+                id = this.Id,
+                amount = this.Amount,
+                link = this.Link
+            };
+            return mpButton;
+        }
+
+
+    }
+}
